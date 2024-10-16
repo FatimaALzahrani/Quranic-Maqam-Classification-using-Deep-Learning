e04c4ba4ac7e7fb4b653580b3b9e54e8fd107b2f
Fatima Mohammed  Alzahrani <107775566+FatimaALzahrani@users.noreply.github.com>
2024-10-17 00:27:15 +0300

    Finish

--- app.py
+++ app.py
@@ -72,5 +72,6 @@ def predict():
 
     return jsonify({'maqam': translated_maqam})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
